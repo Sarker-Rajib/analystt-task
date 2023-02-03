b4889e2bea4f35d15a9c2cbeb12c9bffd0ff93c3
Rajib Sarker <redhat0593@gmail.com>
2023-02-03 23:28:02 +0600

    details added

--- src/Components/UserCard/UserCard.js
+++ src/Components/UserCard/UserCard.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 const UserCard = ({ user }) => {
     const { id, name, address, company } = user;
+    const [details, setDetails] = useState();
 
     const handleDetails = (id) => {
-        fetch()
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then(res => res.json())
+            .then(data => setDetails(data))
     };
+
+    console.log(details);
+
     return (
         <div className='col-12 p-3'>
             <div className="row border p-2">
@@ -26,10 +32,53 @@ const UserCard = ({ user }) => {
                     <p>{address?.street}</p>
                 </div>
                 <div className="col-md-4 col-xl-2">
-                    <Button
-                        onClick={() => handleDetails(id)}
-                        className='m-1'>View Details</Button>
+                    {
+                        details ?
+                            <Button
+                                onClick={() => setDetails(null)}
+                                className='m-1 btn-warning'>View Less</Button>
+                            :
+                            <Button
+                                onClick={() => handleDetails(id)}
+                                className='m-1'>View Details</Button>
+                    }
                 </div>
+
+                {
+                    details && <>
+                        <div>
+                            <hr />
+                            <div className="row">
+                                <div className="col-6">
+                                    <div className="mb-2">
+                                        <h6>Contact person</h6>
+                                        <p>{details.name}</p>
+                                    </div>
+                                    <div className="mb-2">
+                                        <h6>email</h6>
+                                        <p>{details.email}</p>
+                                    </div>
+                                    <div className="mb-2">
+                                        <h6>Phone</h6>
+                                        <p>{details.phone}</p>
+                                    </div>
+                                </div>
+                                <div className="col-6">
+                                    <div className="mb-2">
+                                        <h6>Address</h6>
+                                        <p>{details?.address?.street}</p>
+
+                                    </div>
+
+                                    <div className="mb-2">
+                                        <h6>City</h6>
+                                        <p>{details?.address?.city}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                }
             </div>
         </div>
     );
